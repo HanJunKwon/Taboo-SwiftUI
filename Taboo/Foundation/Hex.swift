58c06b5ea88090e7360d98d7779120ab4bacbf9a
kwonhanjun <skclaqks11@naver.com>
2026-07-27 21:18:21 +0900

    Taboo 기본 색상 코드 추가

---
+++ Taboo/Foundation/Hex.swift
@@ -0,0 +1,19 @@
+//
+//  Hex.swift
+//  Taboo
+//
+//  Created by 권한준 on 7/27/26.
+//
+import SwiftUI
+
+public extension Color {
+    init(hex: UInt, alpha: Double = 1.0) {
+        self.init(
+            .sRGB,
+            red:   Double((hex >> 16) & 0xFF) / 255,
+            green: Double((hex >> 8) & 0xFF) / 255,
+            blue:  Double(hex & 0xFF) / 255,
+            opacity: alpha
+        )
+    }
+}
